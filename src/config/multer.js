d4ae6e16e49c86a10c8e3a87ca8e201fc1872230
Dailton Bastos <dailtonbastos@gmail.com>
2021-02-08 00:19:54 -0300

    feat(upload): add multer config

---
+++ src/config/multer.js
@@ -0,0 +1,24 @@
+import multer from 'multer';
+import { uuid } from 'uuidv4';
+import { extname, resolve } from 'path';
+
+const uploadFolder = resolve(__dirname, '..', '..', 'temp', 'uploads');
+
+export default {
+  storage: multer.diskStorage({
+    destination: uploadFolder,
+    filename: (_, file, callback) => {
+      return callback(null, `${uuid()}-${extname(file.originalname)}`);
+    },
+  }),
+
+  fileFilter: (_, file, callback) => {
+    const isAccepted = ['image/png', 'image/jpg', 'image/jpeg'];
+
+    isAccepted.find((acceptedFormat) => acceptedFormat === file.mimetype);
+
+    if (isAccepted) callback(null, true);
+
+    return callback(null, false);
+  },
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from '../config/multer';
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
 import UserController from '../app/controllers/UserController';
 import UserValidator from '../app/validators/user';
 
-routes.post('/', new UserValidator().create, new UserController().create);
+routes
+  .post('/', new UserValidator().create, new UserController().create)
+  .post('/avatar', upload.single('avatar'), (req, res) =>
+    res.json({ ok: true })
+  );
 
 export default routes;
