2ba51943bff36083782401e321ef055944a61e3a
Dailton Bastos <dailtonbastos@gmail.com>
2021-02-16 23:02:04 -0300

    fix(user validator): check if user exists before update

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -48,6 +48,10 @@ class UserValidate {
 
     const user = await ModelUser.findByPk(id);
 
+    if (!user) {
+      return res.status(400).json({ error: 'User not found!' });
+    }
+
     if (email && email !== user.email) {
       const userExists = await ModelUser.findOne({ where: { email } });
 
