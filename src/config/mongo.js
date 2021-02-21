df84e8bc70fd203bbe0c9a3ea22ed15b7f5a72f5
Dailton Bastos <dailtonbastos@gmail.com>
2021-02-21 11:53:59 -0300

    feat: add mongodb config

---
+++ src/config/mongo.js
@@ -0,0 +1,11 @@
+export default {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+  autoIndex: false,
+  poolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4,
+};
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,8 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
 import databaseConfig from '../config/database';
+import mongoConfig from '../config/mongo';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -9,6 +12,7 @@ const models = [User, File];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -19,6 +23,10 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, mongoConfig);
   }
 }
 
